e7f2c371a158d14ea415012716c3b30602dd1edf
Bruna <bruna.dall9741@gmail.com>
2022-11-24 15:59:53 -0300

    Tarefa 2 - Views

--- teams/views.py
+++ teams/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView, Request, Response, status
+from django.forms.models import model_to_dict
+from teams.models import Team
 
-# Create your views here.
+
+class TeamView(APIView):
+    def post(self, request: Request):
+        team_data = request.data
+        team = Team.objects.create(**team_data)
+
+        return Response(model_to_dict(team), status=status.HTTP_201_CREATED)
+
+    def get(self, request: Request):
+        teams = Team.objects.all()
+        teams_dict = [model_to_dict(team) for team in teams]
+
+        return Response(teams_dict, status=status.HTTP_200_OK)
