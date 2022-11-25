19cdf1373f83d79ba55c16646197fb615848c630
Bruna <bruna.dall9741@gmail.com>
2022-11-25 13:45:35 -0300

    Tarefa 3 - Finalizando Rotas

--- teams/views.py
+++ teams/views.py
@@ -15,3 +15,42 @@ class TeamView(APIView):
         teams_dict = [model_to_dict(team) for team in teams]
 
         return Response(teams_dict, status=status.HTTP_200_OK)
+
+
+class TeamByIdView(APIView):
+    def get(self, request: Request, team_id):
+        try:
+            team = Team.objects.get(pk=team_id)
+        except Team.DoesNotExist:
+            return Response(
+                {"message": "Team not found"}, status=status.HTTP_404_NOT_FOUND
+            )
+
+        return Response(model_to_dict(team), status=status.HTTP_200_OK)
+
+    def patch(self, request: Request, team_id: int):
+        try:
+            team = Team.objects.get(pk=team_id)
+        except Team.DoesNotExist:
+            return Response(
+                {"message": "Team not found"}, status=status.HTTP_404_NOT_FOUND
+            )
+
+        for key, value in request.data.items():
+            setattr(team, key, value)
+
+        team.save()
+
+        return Response(model_to_dict(team), status=status.HTTP_200_OK)
+
+    def delete(self, request: Request, team_id: int):
+        try:
+            team = Team.objects.get(pk=team_id)
+        except Team.DoesNotExist:
+            return Response(
+                {"message": "Team not found"}, status=status.HTTP_404_NOT_FOUND
+            )
+
+        team.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
